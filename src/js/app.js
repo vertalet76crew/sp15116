43e174e185177a27d3fd14593d4cf7ef20c9b266
vertalet76crew <vertalet76crew@gmail.com>
2021-01-07 17:16:25 +0400

    WIP

--- src/js/app.js
+++ src/js/app.js
@@ -4,13 +4,15 @@ import $ from 'jquery';
 
 import menu from './blocks/menu';
 import formRequest from './blocks/form-request';
-import 'jquery-ui-bundle';
+// import "jquery-ui-bundle";
+import './blocks/responsiveTabs';
+
 $(function() {
     const patternUserAgent = /iPhone|iPod|iPad|iOS|android/i;
     const isMobile = !!navigator.userAgent.match(patternUserAgent);
     menu(isMobile);
     formRequest(isMobile);
-    $('#tabs').tabs({
-        active: 1
+    $('#tabs').responsiveTabs({
+        startCollapsed: 'accordion'
     });
 });
--- src/js/blocks/menu.js
+++ src/js/blocks/menu.js
@@ -1,34 +1,49 @@
 import $ from 'jquery';
-import 'jquery-ui-bundle';
+
+// import "jquery-ui-bundle";
 // import "jquery-ui-bundle/jquery-ui.css";
 function menu() {
     const $dropdown = $('[data-target="dropdown"]');
     const $products = $('[data-target="products-panel"]');
     const $solutions = $('[data-target="solutions-panel"]');
     const $resources = $('[data-target="resources-panel"]');
+    const $tabProducts = $('[data-target="products"]');
+    const $tabSolutions = $('[data-target="solutions"]');
+    const $tabResources = $('[data-target="resources"]');
+    let isProducts = false;
+    let isSolution = false;
+    let isResouces = false;
 
     $('body').on('click', function(e) {
         if (!$(e.target).closest('[data-target="dropdown"]').length) {
             if ($(e.target).closest('[data-target="products"]').length) {
-                $dropdown.addClass('show');
-                $products.removeClass('hide');
-                $solutions.addClass('hide');
-                $resources.addClass('hide');
+                $tabProducts.toggleClass('active');
+                $tabSolutions.removeClass('active');
+                $tabResources.removeClass('active');
+                $tabProducts.find('[data-target="dropdown"]').toggleClass('show');
+                $tabSolutions.find('[data-target="dropdown"]').removeClass('show');
+                $tabResources.find('[data-target="dropdown"]').removeClass('show');
             } else if ($(e.target).closest('[data-target="solutions"]').length) {
-                $dropdown.addClass('show');
-                $solutions.removeClass('hide');
-                $resources.addClass('hide');
-                $products.addClass('hide');
+                $tabSolutions.toggleClass('active');
+                $tabProducts.removeClass('active');
+                $tabResources.removeClass('active');
+                $tabSolutions.find('[data-target="dropdown"]').toggleClass('show');
+                $tabProducts.find('[data-target="dropdown"]').removeClass('show');
+                $tabResources.find('[data-target="dropdown"]').removeClass('show');
             } else if ($(e.target).closest('[data-target="resources"]').length) {
-                $dropdown.addClass('show');
-                $resources.removeClass('hide');
-                $solutions.addClass('hide');
-                $products.addClass('hide');
+                $tabResources.toggleClass('active');
+                $tabSolutions.removeClass('active');
+                $tabProducts.removeClass('active');
+                $tabResources.find('[data-target="dropdown"]').toggleClass('show');
+                $tabProducts.find('[data-target="dropdown"]').removeClass('show');
+                $tabSolutions.find('[data-target="dropdown"]').removeClass('show');
             } else {
-                $dropdown.removeClass('show');
-                $resources.addClass('hide');
-                $solutions.addClass('hide');
-                $products.addClass('hide');
+                $tabResources.removeClass('active');
+                $tabSolutions.removeClass('active');
+                $tabProducts.removeClass('active');
+                $tabResources.find('[data-target="dropdown"]').removeClass('show');
+                $tabProducts.find('[data-target="dropdown"]').removeClass('show');
+                $tabSolutions.find('[data-target="dropdown"]').removeClass('show');
             }
         }
     });
