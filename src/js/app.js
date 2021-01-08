3797a05695f4c5c9ae73a488dd0be4646ec40a09
vertalet76crew <vertalet76crew@gmail.com>
2021-01-08 17:53:01 +0400

    WIP

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,18 @@
-import 'polyfill-array-includes';
-import 'element-closest-polyfill';
-import $ from 'jquery';
+import "polyfill-array-includes";
+import "element-closest-polyfill";
+import $ from "jquery";
 
-import menu from './blocks/menu';
-import formRequest from './blocks/form-request';
+import menu from "./blocks/menu";
+import formRequest from "./blocks/form-request";
 // import "jquery-ui-bundle";
-import './blocks/responsiveTabs';
+import "./blocks/responsiveTabs";
 
-$(function() {
-    const patternUserAgent = /iPhone|iPod|iPad|iOS|android/i;
-    const isMobile = !!navigator.userAgent.match(patternUserAgent);
-    menu(isMobile);
-    formRequest(isMobile);
-    $('#tabs').responsiveTabs({
-        startCollapsed: 'accordion'
-    });
+$(function () {
+  const patternUserAgent = /iPhone|iPod|iPad|iOS|android/i;
+  const isMobile = !!navigator.userAgent.match(patternUserAgent);
+  menu(isMobile);
+  formRequest(isMobile);
+  $("#tabs").responsiveTabs({
+    startCollapsed: false,
+  });
 });
